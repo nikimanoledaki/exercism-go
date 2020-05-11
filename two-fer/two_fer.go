c6c1bb5528a2b126190dfcd4a7fe9f1b0a775f2b
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-11 14:42:01 +0100

    Refactored

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,11 +1,12 @@
+// Package twofer provides function for sharing with you and me.
 package twofer
 
+import "fmt"
+
+// ShareWith prints "One for X, one for me." where x is the name provided.
 func ShareWith(name string) string {
-	// ShareWith returns "One for X, one for me." where X is the name,
-	// and returns the string if the name is missing.
 	if name == "" {
-		return "One for you, one for me."
-	} else {
-		return "One for " + name + ", one for me."
+		name = "you"
 	}
+	return fmt.Sprintf("One for %v, one for me.", name)
 }
