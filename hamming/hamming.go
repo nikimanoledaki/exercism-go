e99bb0f6438f2b7649f49b8aa40483184147eaba
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-12 16:43:14 +0100

    Calculates hamming distance

---
+++ hamming/hamming.go
@@ -0,0 +1,21 @@
+// Package hamming calculates the hamming distance.
+package hamming
+
+import "errors"
+
+// Distance compares two strings and counts the differences.
+func Distance(a, b string) (int, error) {
+	var err error
+	if len(a) != len(b) {
+		return 0, errors.New("length of two strings must be equal")
+	}
+
+	var difference int
+	for index := range a {
+		if a[index] != b[index] {
+			difference++
+		}
+	}
+
+	return difference, err
+}
