811a3b9a63849b1d47c12933641f3c817a06ca15
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-12 20:49:36 +0100

    Leap year

---
+++ leap/leap.go
@@ -0,0 +1,7 @@
+// Package leap evaluates leap year.
+package leap
+
+// IsLeapYear evaluates if year is a leap year.
+func IsLeapYear(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
