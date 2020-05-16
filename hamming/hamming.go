9f6995f94c4ad3d2c597a1ab65f327a6de6e4b98
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-16 17:09:21 +0100

    Type casting string to rune

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -3,12 +3,11 @@ package hamming
 
 import (
 	"errors"
-	"strings"
 )
 
 // Distance compares two strings and counts the differences.
 func Distance(a, b string) (int, error) {
-	charsA, charsB := strings.Split(a, ""), strings.Split(b, "")
+	charsA, charsB := []rune(a), []rune(b)
 	if len(charsA) != len(charsB) {
 		return 0, errors.New("length of two strings must be equal")
 	}
