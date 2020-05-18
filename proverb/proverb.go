8b1478429c83aef9ce585d016e311678e4faebb5
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-18 15:05:42 +0100

    Proverb: Returns the proverb given a slice of strings

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,15 +1,26 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package proverb returns the "for want of a nail" proverb.
 package proverb
 
-// Proverb should have a comment documenting it.
+import (
+	"fmt"
+)
+
+// Proverb returns the proverb given a slice of strings.
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	if len(rhyme) == 0 {
+		return rhyme
+	}
+
+	result := []string{}
+
+	for i, piece := range rhyme {
+		if i < len(rhyme)-1 {
+			result = append(result, fmt.Sprintf("For want of a %s the %s was lost.", piece, rhyme[i+1]))
+		} else {
+			result = append(result, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
+		}
+	}
+
+	return result
+
 }
