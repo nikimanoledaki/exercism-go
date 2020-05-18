c49582fd94be1d9800c65087e581d5b78eeac15c
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-18 12:09:42 +0100

    Raindrops: Converts number into sound string

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,1 +1,25 @@
 package raindrops
+
+import "fmt"
+
+// Convert converts a number intro a string.
+func Convert(number int) string {
+	var sounds string
+	if number%3 == 0 {
+		sounds += "Pling"
+	}
+
+	if number%5 == 0 {
+		sounds += "Plang"
+	}
+
+	if number%7 == 0 {
+		sounds += "Plong"
+	}
+
+	if number%3 != 0 && number%5 != 0 && number%7 != 0 {
+		sounds += fmt.Sprintf("%d", number)
+	}
+
+	return sounds
+}
