e676b2627633cd2ecfcad6a8d1e3344fc691dc45
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-27 19:49:24 +0100

    Refactors var out of function

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,15 +1,14 @@
 package strand
 
+var complements = map[rune]string{
+	'C': "G",
+	'G': "C",
+	'T': "A",
+	'A': "U",
+}
+
 // ToRNA returns a RNA complement (per RNA transcription) given a DNA strand.
 func ToRNA(dna string) string {
-
-	var complements = map[rune]string{
-		'C': "G",
-		'G': "C",
-		'T': "A",
-		'A': "U",
-	}
-
 	var rna string
 	for _, n := range dna {
 		rna += complements[n]
