0093478aca71ac2ee5376345bbbc8be8bdbffec4
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-27 18:20:46 +0100

    Returns error if not a nucleotide

--- nucleotide-count/nucleotide_count.go
+++ nucleotide-count/nucleotide_count.go
@@ -1,13 +1,21 @@
 package dna
 
+import "errors"
+
 // DNA is a list of nucleotides.
 type DNA string
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
-type Histogram map[byte]int
+type Histogram map[rune]int
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 func (d DNA) Counts() (Histogram, error) {
 	h := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+	for _, n := range d {
+		if n != 'A' && n != 'C' && n != 'G' && n != 'T' {
+			return h, errors.New("Not valid")
+		}
+		h[n]++
+	}
 	return h, nil
 }
