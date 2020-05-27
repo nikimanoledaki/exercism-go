d6979340fc8d8e5fadbc27bb1f3ff8e8af8819c7
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-27 14:52:09 +0100

    ToRNA returns a RNA compelement given a DNA strand

---
+++ rna-transcription/rna_transcription.go
@@ -0,0 +1,19 @@
+package strand
+
+// ToRNA returns a RNA complement (per RNA transcription) given a DNA strand.
+func ToRNA(dna string) string {
+
+	var complements = map[rune]string{
+		'C': "G",
+		'G': "C",
+		'T': "A",
+		'A': "U",
+	}
+
+	var rna string
+	for _, n := range dna {
+		rna += complements[n]
+	}
+
+	return rna
+}
