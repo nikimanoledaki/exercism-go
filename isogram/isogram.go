291a48aaa88e8c5cde4b6312a6c44ba8cea43c81
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-28 09:28:19 +0100

    Refactored

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -4,22 +4,15 @@ import "unicode"
 
 // IsIsogram returns true if inputted word is an isogram.
 func IsIsogram(word string) bool {
-	chars := Histogram(word)
-	for k := range chars {
-		if chars[k] > 1 {
+	chars := make(map[rune]int)
+	for _, c := range word {
+		lowerC := unicode.ToLower(c)
+		if unicode.IsLetter(c) {
+			chars[lowerC]++
+		}
+		if chars[lowerC] > 1 {
 			return false
 		}
 	}
 	return true
-}
-
-// Histogram returns a frequency count of characters in word.
-func Histogram(word string) map[rune]int {
-	chars := make(map[rune]int)
-	for _, c := range word {
-		if unicode.IsLetter(c) {
-			chars[unicode.ToLower(c)]++
-		}
-	}
-	return chars
 }
