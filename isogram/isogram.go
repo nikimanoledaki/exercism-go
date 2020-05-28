8c66e0127d82c7a094259abea4c41ae75eb2a515
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-28 09:41:11 +0100

    Refactored

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -7,11 +7,11 @@ func IsIsogram(word string) bool {
 	chars := make(map[rune]int)
 	for _, c := range word {
 		lowerC := unicode.ToLower(c)
+		if chars[lowerC] >= 1 {
+			return false
+		}
 		if unicode.IsLetter(c) {
 			chars[lowerC]++
-		}
-		if chars[lowerC] > 1 {
-			return false
 		}
 	}
 	return true
