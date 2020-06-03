62f671a6d8ad18d51153f52b01aa9de34fc0c74d
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-06-03 14:36:38 +0100

    refactored

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -7,7 +7,7 @@ import (
 
 // Valid checks if a given string is valid according to the Luhn formula.
 func Valid(s string) bool {
-	s = strings.Replace(s, " ", "", -1)
+	s = strings.ReplaceAll(s, " ", "")
 	length := len(s)
 	if length < 2 {
 		return false
@@ -16,17 +16,20 @@ func Valid(s string) bool {
 	sum := 0
 	for i := 0; i < length; i++ {
 		n, err := strconv.Atoi(string(s[length-1-i]))
+
 		if err != nil {
 			return false
 		}
-		if i%2 == 0 {
+
+		sum += n
+
+		if i%2 != 0 {
 			sum += n
-		} else {
+
 			double := n * 2
 			if double > 9 {
-				double -= 9
+				sum -= 9
 			}
-			sum += double
 		}
 	}
 	return sum%10 == 0
