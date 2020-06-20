68eed76b8cbd5653028f1144fa4281a09d9f878e
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-06-20 10:58:57 +0100

    Returns error if input is <= 0 or greater than 64

--- grains/grains.go
+++ grains/grains.go
@@ -1,11 +1,14 @@
 package grains
 
-import "math"
+import (
+	"errors"
+	"math"
+)
 
 // Square returns the current chessboard square as a uint64.
 func Square(square int) (uint64, error) {
-	if square == 1 {
-		return 1, nil
+	if square <= 0 || square > 64 {
+		return 0, errors.New("Input cannot be <= 0 or greater than 64")
 	}
 	o := float64(square)
 	p := math.Pow(2, o-1)
