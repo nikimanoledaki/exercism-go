73c8b732ca72b88fc931365dd751c8d77773f8f3
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-06-20 11:00:47 +0100

    Grains: Passes tests

--- grains/grains.go
+++ grains/grains.go
@@ -8,7 +8,7 @@ import (
 // Square returns the current chessboard square as a uint64.
 func Square(square int) (uint64, error) {
 	if square <= 0 || square > 64 {
-		return 0, errors.New("Input cannot be <= 0 or greater than 64")
+		return 0, errors.New("Input cannot be less than or equal to 0 or greater than 64")
 	}
 	o := float64(square)
 	p := math.Pow(2, o-1)
@@ -16,9 +16,7 @@ func Square(square int) (uint64, error) {
 	return q, nil
 }
 
-// Total says hi.
+// Total returns the total number of grains on the chessboard.
 func Total() uint64 {
-	// p := math.Pow(64, 2)
-	// q := uint64(p)
-	return 0
+	return 18446744073709551615
 }
