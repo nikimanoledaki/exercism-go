4a8e5dd3f1b9924b51a97c9bf7932772dacb5d81
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-06-20 11:14:57 +0100

    Solution with bit shift operator

--- grains/grains.go
+++ grains/grains.go
@@ -2,7 +2,6 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Square returns the current chessboard square as a uint64.
@@ -10,13 +9,10 @@ func Square(square int) (uint64, error) {
 	if square <= 0 || square > 64 {
 		return 0, errors.New("Input cannot be less than or equal to 0 or greater than 64")
 	}
-	o := float64(square)
-	p := math.Pow(2, o-1)
-	q := uint64(p)
-	return q, nil
+	return (1 << uint(square-1)), nil
 }
 
 // Total returns the total number of grains on the chessboard.
 func Total() uint64 {
-	return 18446744073709551615
+	return 1<<64 - 1
 }
