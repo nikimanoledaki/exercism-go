7544042a41bcfae1754b066ca8c93db1bfff7d73
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-07-29 09:28:30 +0100

    Clock: Passes test

---
+++ clock/clock.go
@@ -0,0 +1,49 @@
+package clock
+
+import "fmt"
+
+// Clock struct has an hour and a minute field, where both are type integer.
+type Clock struct {
+	hour   int
+	minute int
+}
+
+// New creates a new clock struct.
+func New(hour, minute int) Clock {
+	return Clock{hour, minute}.CorrectTime()
+}
+
+// String converts clock struct to type string.
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+}
+
+// Add adds given minutes of type int to a Clock struct's minutes field.
+func (c Clock) Add(minutes int) Clock {
+	c.minute += minutes
+	return c.CorrectTime()
+}
+
+// Subtract subtracts given minutes of type int to a Clock struct's minutes field.
+func (c Clock) Subtract(minutes int) Clock {
+	c.minute -= minutes
+	return c.CorrectTime()
+}
+
+// CorrectTime adjusts the time according to the conventions for writing the time.
+func (c Clock) CorrectTime() Clock {
+	for c.minute < 0 {
+		c.minute += 60
+		c.hour--
+	}
+
+	for c.hour < 0 {
+		c.hour += 24
+	}
+
+	c.hour += int(c.minute / 60)
+	c.minute = c.minute % 60
+	c.hour = c.hour % 24
+
+	return c
+}
